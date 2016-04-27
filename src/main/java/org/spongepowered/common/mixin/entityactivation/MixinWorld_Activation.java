666fb2d0318789df9dd1e6a8725bc748fc2fb1c2
bloodshot <jdroque@gmail.com>
2016-04-26 22:27:15 -0400

    Improve entity performance in many areas.
    * Refactored and improved entity activation module.
      - Mods can be added to config and overridden to change defaults or disabled to ignore activation rules.
      - Each entity type can now be overridden. For example, you can override skeletons to have a range of 16 blocks while the other
        monsters stick to a default of 32.
      - Mod and Entity data can be auto-populated by toggling the 'auto-populate' setting to true in config. You can manually
        change the config then simply use the '/sponge reload -g' command to reload global config(use -w for worlds).
        Note: SpongeVanilla users can rejoice as this module is now in common.
    
    * Added new entity collision module to improve performance in areas where too many entities are on top of eachother such as a 1x1
      spawn pen.
      - The same features of entity activation can be applied here with the addition of blocks. The default amount of collision
        targets that a singleentity or block can collide with is 8. There are a few exceptions that have been added to config to try
        and match vanilla experience.
      Note: This will probably break some vanilla features so be sure to report any issues so we may update the default settings.
    
    * NotifyNeighborBlockEvent will no longer have a BlockSnapshot as a root cause. Instead, a BlockState will be passed to improve
      performance. To determine the position of the source block, simply check the opposite direction of a neighbor block.
    * Don't track air block neighbor notifications.
    * Active world configs are now stored within the World object to improve performance. If you change an active world config, make
      sure to use the '/sponge reload' command so the active configs get updated.
    * World configs will now be saved when worlds unload to avoid losing any changes you may have added.

---
+++ src/main/java/org/spongepowered/common/mixin/entityactivation/MixinWorld_Activation.java
@@ -0,0 +1,69 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.common.mixin.entityactivation;
+
+import net.minecraft.profiler.Profiler;
+import org.spongepowered.api.util.annotation.NonnullByDefault;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.common.interfaces.world.IMixinWorld;
+import org.spongepowered.common.mixin.plugin.entityactivation.ActivationRange;
+import org.spongepowered.common.mixin.plugin.entityactivation.interfaces.IModData_Activation;
+
+@NonnullByDefault
+@Mixin(net.minecraft.world.World.class)
+public abstract class MixinWorld_Activation implements IMixinWorld {
+
+    @Shadow @Final public Profiler theProfiler;
+
+    @Shadow public abstract boolean isAreaLoaded(int xStart, int yStart, int zStart, int xEnd, int yEnd, int zEnd, boolean allowEmpty);
+    @Shadow public abstract boolean isChunkLoaded(int x, int z, boolean allowEmpty);
+
+    @Inject(method = "updateEntities()V", at = @At(value = "INVOKE_STRING",
+            target = "Lnet/minecraft/profiler/Profiler;endStartSection(Ljava/lang/String;)V", args = {"ldc=regular"}))
+    private void onInvokeProfiler(CallbackInfo ci) {
+        if (!((net.minecraft.world.World) (Object) this).isRemote) {
+            ActivationRange.activateEntities(((net.minecraft.world.World) (Object) this));
+        }
+    }
+
+    @Inject(method = "updateEntityWithOptionalForce", at = @At(value = "INVOKE",
+            target = "Lnet/minecraftforge/event/ForgeEventFactory;canEntityUpdate(Lnet/minecraft/entity/Entity;)Z",
+            shift = At.Shift.BY, by = 3, ordinal = 0, remap = false), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
+    public void onUpdateEntityWithOptionalForce(net.minecraft.entity.Entity entity, boolean forceUpdate, CallbackInfo ci, int i, int j,
+            boolean isForced, int k, boolean canUpdate) {
+        if (!isForced && !ActivationRange.checkIfActive(entity)) { // ignore if forced by forge event update or entity's chunk
+            entity.ticksExisted++;
+            ((IModData_Activation) entity).inactiveTick();
+            ci.cancel();
+        }
+    }
+
+}

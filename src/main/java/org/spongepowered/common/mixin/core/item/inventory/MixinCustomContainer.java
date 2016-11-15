70692cb489590ebbef8e3d81774c2a63d2b49ef5
Anselm Brehme <development.anselm@breh.me>
2016-11-15 20:21:55 +0100

    Container providing Property also fix MixinCustomContainer

--- src/main/java/org/spongepowered/common/mixin/core/item/inventory/MixinCustomContainer.java
+++ src/main/java/org/spongepowered/common/mixin/core/item/inventory/MixinCustomContainer.java
@@ -24,21 +24,16 @@
  */
 package org.spongepowered.common.mixin.core.item.inventory;
 
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.InventoryBasic;
 import net.minecraft.item.ItemStack;
-import org.spongepowered.api.item.inventory.Carrier;
 import org.spongepowered.api.item.inventory.Inventory;
 import org.spongepowered.api.item.inventory.InventoryArchetype;
-import org.spongepowered.api.item.inventory.InventoryProperty;
-import org.spongepowered.api.item.inventory.type.CarriedInventory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.common.item.inventory.adapter.InventoryAdapter;
 import org.spongepowered.common.item.inventory.adapter.impl.MinecraftInventoryAdapter;
 import org.spongepowered.common.item.inventory.custom.CustomContainer;
 import org.spongepowered.common.item.inventory.custom.CustomContainerLens;
@@ -46,14 +41,9 @@ import org.spongepowered.common.item.inventory.custom.CustomInventory;
 import org.spongepowered.common.item.inventory.custom.CustomLens;
 import org.spongepowered.common.item.inventory.lens.Fabric;
 import org.spongepowered.common.item.inventory.lens.Lens;
-import org.spongepowered.common.item.inventory.lens.SlotProvider;
 import org.spongepowered.common.item.inventory.lens.impl.MinecraftFabric;
 import org.spongepowered.common.item.inventory.lens.impl.collections.SlotCollection;
-import org.spongepowered.common.item.inventory.lens.impl.fabric.DefaultInventoryFabric;
 import org.spongepowered.common.item.inventory.observer.InventoryEventArgs;
-
-import java.util.Map;
-import java.util.Optional;
 
 @Mixin(CustomContainer.class)
 public abstract class MixinCustomContainer implements MinecraftInventoryAdapter, Inventory {
@@ -65,7 +55,7 @@ public abstract class MixinCustomContainer implements MinecraftInventoryAdapter,
     private CustomContainerLens lens;
 
     @Inject(method = "<init>*", at = @At("RETURN"), remap = false)
-    private void onConstructed(EntityPlayer player, CustomInventory inventory, CallbackInfo ci) {
+    private void onConstructed(InventoryPlayer inventoryPlayer, CustomInventory inventory, CallbackInfo ci) {
         this.inventory = MinecraftFabric.of(this);
         // CustomInventory + Main PlayerInventory + HotBar PlayerInventory
         this.slots = new SlotCollection.Builder().add(inventory.getSizeInventory()).add(36).build();
